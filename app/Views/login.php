c640ec90fefaec550ce2628c5e4a5232218195d6
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-04-03 14:12:35 +0700

    fix validation final

--- app/Views/login.php
+++ app/Views/login.php
@@ -9,11 +9,21 @@
                     <img src="<?= base_url() ?>assets/images/logo.png" class="img-fluid" alt="Sample image">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                    <!-- membuat flash data dari router setelah di save -->
+                    <?php if (session()->getFlashdata('pesan')) : ?>
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <?= session()->getFlashdata('pesan'); ?>
+                            <!-- <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button> -->
+                        </div>
+                    <?php endif; ?>
+                    <!-- end flash data -->
                     <form action="<?= base_url() ?>login/auth" method="post">
                         <? csrf_field() ?>
                         <div class="input-field" style="padding-bottom: 30px;">
                             <i class='bx bx-user'></i>
-                            <input type="text" id="nama" name="nama" oninput="this.value = this.value.toUpperCase()" class="input <?= (validation_show_error('nama')) ? 'is-invalid' : ''; ?>" placeholder="Masukkan Nama Lengkap" />
+                            <input type="text" id="nama" name="nama" oninput="this.value = this.value.toUpperCase()" class="input <?= (validation_show_error('nama')) ? 'is-invalid' : ''; ?>" value="<?= old('nama') ?>" placeholder="Masukkan Nama Lengkap" />
                             <div class="invalid-feedback">
                                 <?= validation_show_error('nama'); ?>
                             </div>
@@ -21,7 +31,7 @@
 
                         <div class="input-field" style="padding-bottom: 30px;">
                             <i class='bx bx-lock-alt'></i>
-                            <input type="password" id="npk" name="npk" class="input <?= (validation_show_error('npk')) ? 'is-invalid' : ''; ?>" placeholder="Masukkan NPK" />
+                            <input type="password" id="npk" name="npk" class="input <?= (validation_show_error('npk')) ? 'is-invalid' : ''; ?>" value="<?= old('npk') ?>" placeholder="Masukkan NPK" />
                             <div class="invalid-feedback">
                                 <?= validation_show_error('npk'); ?>
                             </div>
